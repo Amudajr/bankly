0ba0743b6acebbc6fae83af30a54762fe0fbfc9d
Amudajr <classicallow26@gmail.com>
2023-09-22 08:28:22 +0100

    tyuio

--- lib/views/transaction_page/transaction_page.dart
+++ lib/views/transaction_page/transaction_page.dart
@@ -98,7 +98,7 @@ class TransactionPageState extends ConsumerState<TransactionPage> {
             Card(
               child: Center(
                 child: InputFormField(
-                  hintText: 'Find here',
+                  hintText: 'Find it',
                   onChanged: (val) {
                     if (val != '' || val.isNotEmpty) {
                       setState(() {
